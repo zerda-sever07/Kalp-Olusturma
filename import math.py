641d0bbd3f54e8cab3a80fdbebe65e0664545b0b
Zerda Sever <zerda1343@gmail.com>
2025-12-30 12:00:47 +0300

    Add files via upload

---
+++ import math.py
@@ -0,0 +1,17 @@
+import math
+from turtle import * 
+def kalp(e):
+    return 15*math.sin(e)**3
+def kalpe(e):
+    return 12*math.cos(e)-5*\
+    math.cos(2*e)-2*\
+    math.cos(3*e)-\
+    math.cos(4*e)
+speed(1000)
+bgcolor("black")
+for m in range(6000):
+    goto(kalp(m)*20,kalpe(m)*20)
+    for i in range(5):
+        color("blue")
+        goto(0,0)
+done()
